c7bec74ea0807580e568cc4974c9f270ca1c849a
Mark Gordon <markgordonn@gmail.com>
2018-07-31 16:24:10 +0100

    Add project files.

---
+++ PomodoroGamify/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PomodoroGamify.Startup))]
+namespace PomodoroGamify
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
